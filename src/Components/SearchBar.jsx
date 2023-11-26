b9104bbd155a8e0872761d5393fbeca72eb8d415
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-11-26 11:18:26 +0530

    world-by-currency

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -34,7 +34,7 @@ const SearchBar = () => {
           <FaSearch className="h-5 w-5 text-gray-500" />
         </div>
         <input
-          className="py-3 pl-10 pr-10 border border-gray-300 rounded-md w-96 focus:outline-none focus:border-blue-500 text-center"
+          className="py-3 pl-10 pr-10 border border-gray-300 rounded-md w-96 focus:outline-none focus:border-blue-500 "
           type="text"
           placeholder="Search by Currency INR, EUR, USD"
           value={searchQuery}
